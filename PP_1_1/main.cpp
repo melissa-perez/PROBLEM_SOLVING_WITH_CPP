24cf22b633b30be84f980bda1690085545fb1f93
Melissa Perez <you@example.com>
2018-06-23 14:12:48 -0700

    first three pp problems

---
+++ PP_1_1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a=0;
+    int b=0;
+
+    cout<<"Enter the first integer: ";
+    cin>>a;
+    cout<<"\nEnter the second integer: ";
+    cin>>b;
+    cout<<"The sum of "<<a<< " and "<<b<<" is "<<a+b<<endl;
+    cout<<"The product of "<<a<< " and "<<b<<" is "<<a*b<<endl;
+    cout<<"End of program.\n";
+
+    return 0;
+}
---
+++ PP_1_2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "*************************************************" << endl;
+    cout<<  "\tC C C \t\tS S S S \t!!"<<endl;
+    cout<<  "       C     C         S       S\t!!"<<endl;
+    cout<< "      C               S            \t!!"<<endl;
+    cout<< "     C                 S         \t!!"<<endl;
+    cout<< "     C                  S S S S   \t!!"<<endl;
+    cout<< "     C                          S\t!!"<<endl;
+    cout<< "      C                          S\t!!"<<endl;
+    cout<< "       C     C         S        S\t!!"<<endl;
+    cout<< "\tC C C \t\tS S S S \t00"<<endl;
+
+    cout << "*************************************************" << endl;
+    cout<<endl<<endl;
+    cout<<"Computer Science is Cool Stuff!!!"<<endl;
+    return 0;
+}
