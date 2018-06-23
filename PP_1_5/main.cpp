61f397084588f3e992778283b9295fbb5b92ff13
Melissa Perez <you@example.com>
2018-06-23 14:35:43 -0700

    prob 5

---
+++ PP_1_5/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char user_input='A';
+
+    cout<<"Enter a letter you wish to display: ";
+    cin>>user_input;
+    cout<<"\t"<<user_input<<" "<<user_input<<" "<<user_input<<endl;
+    cout<<"      "<<user_input<<"       "<<user_input<<endl;
+    cout<<"     "<<user_input<<endl;
+    cout<<"     "<<user_input<<endl;
+    cout<<"     "<<user_input<<endl;
+    cout<<"     "<<user_input<<endl;
+    cout<<"     "<<user_input<<endl;
+    cout<<"      "<<user_input<<"       "<<user_input<<endl;
+    cout<<"\t"<<user_input<<" "<<user_input<<" "<<user_input<<endl;
+   return 0;
+}
